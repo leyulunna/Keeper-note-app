207dbbe2c77507b1141cdb8177ea23914c19bfaf
Lena Zhang <64572479+leyulunna@users.noreply.github.com>
2020-12-22 18:36:40 +0800

    fix: <a> tag in Footer sextion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,11 @@ function Footer() {
   return (
     <footer>
       <p>Copyright ⓒ {year}
-      <a className="fab fa-github" href="https://github.com/leyulunna/keeper-note-app" target="_blank"></a>
+      <a className="fab fa-github" rel="noreferrer" href="https://github.com/leyulunna/keeper-note-app" target="_blank">{null}</a>
       </p>
     </footer>
   );
 }
 
 export default Footer;
+
